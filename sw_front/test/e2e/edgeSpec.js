eaa59291c565ceef84aadc17901f4ecbb5edf44c
Daniela Baron <baron.daniela@gmail.com>
2014-12-27 12:36:20 -0500

    navigation e2e testing

--- sw_front/test/e2e/edgeSpec.js
+++ sw_front/test/e2e/edgeSpec.js
@@ -1,8 +1,16 @@
 describe('Edges Application', function () {
 
-  it('Homepage', function() {
+  beforeEach(function() {
     browser.get('http://localhost:9000');
+  });
+
+  it('Homepage has a title', function() {
     expect(browser.getTitle()).toEqual('Savage Worlds Database');
-  })
+  });
+
+  it('Homepage displays a welcome message', function() {
+    var welcomeEl = element(by.css('.jumbotron'));
+    expect(welcomeEl.getText()).toContain('Welcome to Savage Worlds!');
+  });
 
 });
---
+++ sw_front/test/e2e/navigationSpec.js
@@ -0,0 +1,33 @@
+describe('Navigation Menu', function () {
+
+  beforeEach(function () {
+    browser.get('http://localhost:9000');
+  });
+
+  var verifyActiveLink = function(expectedLinkText) {
+    var activeEl = element(by.css('.active'));
+    expect(activeEl.getText()).toEqual(expectedLinkText);
+  }
+
+  it('Home link is active by default', function () {
+    verifyActiveLink('Home');
+  });
+
+  it('Displays navigation items', function() {
+    var navItems = element.all(by.css('.nav li'));
+    expect(navItems.get(0).getText()).toEqual('Home');
+    expect(navItems.get(1).getText()).toEqual('Edges');
+  });
+
+  it('Changes active link when navigation item is selected', function() {
+    var navItems = element.all(by.css('.nav li'));
+    var edgesNavItem = navItems.get(1);
+    var homeNavItem = navItems.get(0);
+
+    edgesNavItem.click();
+    verifyActiveLink('Edges');
+
+    homeNavItem.click();
+    verifyActiveLink('Home');
+  })
+});
