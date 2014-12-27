138d62907d00f1dc9ee5b766f9d5db62b6ea7321
Daniela Baron <baron.daniela@gmail.com>
2014-12-27 07:09:14 -0500

    enhance navigation function and more unit tests

--- sw_front/app/scripts/controllers/navigation.js
+++ sw_front/app/scripts/controllers/navigation.js
@@ -3,6 +3,7 @@
 angular.module('swFrontApp')
   .controller('NavigationCtrl', function ($scope, $location) {
     $scope.isActive = function(path) {
-      return path === $location.path();
+      var currentPath = $location.path().split('/')[1].split('?')[0];
+      return currentPath === path.split('/')[1];
     };
   });
--- sw_front/test/spec/controllers/navigation.js
+++ sw_front/test/spec/controllers/navigation.js
@@ -22,16 +22,25 @@ describe('Controller: NavigationCtrl', function () {
 
     it('Returns true when input path is the same as $location path', function() {
       spyOn(location, 'path').and.returnValue('/edges');
-      var result = scope.isActive('/edges');
-      expect(result).toBe(true);
+      expect(scope.isActive('/edges')).toBe(true);
       expect(location.path).toHaveBeenCalled();
     });
 
     it('Returns false when input path is different from $location path', function() {
       spyOn(location, 'path').and.returnValue('/edges');
-      var result = scope.isActive('/');
-      expect(result).toBe(false);
+      expect(scope.isActive('/')).toBe(false);
+      expect(location.path).toHaveBeenCalled();
+    });
 
+    it('Returns true when input path starts with the same word as $location path', function() {
+      spyOn(location, 'path').and.returnValue('/test/1/show');
+      expect(scope.isActive('/test')).toBe(true);
+      expect(location.path).toHaveBeenCalled();
+    });
+
+    it('Returns true when input path starts with the same word as $location path followed by query string', function() {
+      spyOn(location, 'path').and.returnValue('/test?id=1');
+      expect(scope.isActive('/test')).toBe(true);
       expect(location.path).toHaveBeenCalled();
     });
 
