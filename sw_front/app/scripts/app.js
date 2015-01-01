da0f206eb5e2a906cd21bba98b13216161b10bf5
Daniela Baron <baron.daniela@gmail.com>
2015-01-01 07:53:42 -0500

    cleanup app wrt http interceptor

--- sw_front/app/scripts/app.js
+++ sw_front/app/scripts/app.js
@@ -7,59 +7,55 @@ angular
     'ngRoute',
     'ngSanitize'
   ])
-  .config(function ($routeProvider, $locationProvider, $httpProvider, $provide) {
-
-  //================================================
-  // Check if the user is logged in
-  // https://raw.githubusercontent.com/Anomen/AuthenticationAngularJS/master/public/javascripts/app.js
-  //================================================
-  var checkLoggedin = function($q, $timeout, $http, $location, $rootScope) {
-    // Initialize a new promise
-    var deferred = $q.defer();
-
-    // Make an AJAX call to check if the user is logged in
-    $http.get('/api/loggedin').success(function(user) {
-      // Authenticated
-      if (user !== '0') {
-        $timeout(deferred.resolve, 0);
-      }
-      else {
-        // Not Authenticated
-        $rootScope.message = 'You need to log in.';
-        $timeout(function() {
-          deferred.reject();
-        }, 0);
-        $location.url('/login');
-      }
-    });
-
-    return deferred.promise;
-  };
-  //================================================
 
   //================================================
   // Add an interceptor for AJAX errors
   // https://raw.githubusercontent.com/Anomen/AuthenticationAngularJS/master/public/javascripts/app.js
   // http://stackoverflow.com/questions/23804981/alternative-of-httpprovider-responseinterceptors
   //================================================
-    $provide.factory('myHttpInterceptor', function($q, $location) {
-      return {
-        // Success
-        'response': function(response) {
-          return response;
-        },
-        // Error: check the error status to get only the 401
-       'responseError': function(rejection) {
-          if (rejection.status === 401 && $location.url() !== '/login') {
-            $location.url('/login');
-          } else {
-            return $q.reject(rejection);
-          }
+  .factory('myHttpInterceptor', function($q, $location) {
+    return {
+      // Success
+      'response': function(response) {
+        return response;
+      },
+      // Error: check the error status to get only the 401
+     'responseError': function(rejection) {
+        if (rejection.status === 401 && $location.url() !== '/login') {
+          $location.url('/login');
+        } else {
+          return $q.reject(rejection);
         }
-      };
-    });
+      }
+    };
+  })
+
+  .config(function ($routeProvider, $locationProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('myHttpInterceptor');
+
+    //================================================
+    // Check if the user is logged in
+    // https://raw.githubusercontent.com/Anomen/AuthenticationAngularJS/master/public/javascripts/app.js
+    //================================================
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope) {
+      var deferred = $q.defer();
+      $http.get('/api/loggedin').success(function(user) {
+        if (user !== '0') {
+          // Authenticated
+          $timeout(deferred.resolve, 0);
+        }
+        else {
+          // Not Authenticated
+          $rootScope.message = 'You need to log in.';
+          $timeout(function() {
+            deferred.reject();
+          }, 0);
+          $location.url('/login');
+        }
+      });
+      return deferred.promise;
+    };
 
     $routeProvider
       .when('/', {
